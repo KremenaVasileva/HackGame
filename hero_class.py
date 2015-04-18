b478f20804bb821e7619212038fef7fc9a864a82
Kremena Vasleva <k.vasileva695@gmail.com>
2015-04-18 12:29:27 +0300

    Finishing Hero class and tests

--- hero_class.py
+++ hero_class.py
@@ -1,33 +1,24 @@
-class Hero:
+from creature_class import Creature
+from Weapon import Weapon
+from Spell import Spell
+
+
+class Hero(Creature):
     def __init__(self, **kwargs):
         self.name = kwargs['name']
         self.title = kwargs['title']
         self.health = kwargs['health']
         self.mana = kwargs['mana']
         self.mana_regeneration_rate = kwargs['mana_regeneration_rate']
+
         self.starting_health = kwargs['health']
         self.starting_mana = kwargs['mana']
 
+        self.current_spell = None
+        self.current_weapon = None
+
     def known_as(self):
         return "{} known as the {}".format(self.name, self.title)
-
-    def get_health(self):
-        return self.health
-
-    def get_mana(self):
-        return self.mana
-
-    def is_alive(self):
-        if self.health <= 0:
-            return False
-        else:
-            return True
-
-    def can_cast(self):
-        pass
-
-    def take_damage(self, damage_points):
-        return max(0, self.health - damage_points)
 
     def take_healing(self, healing_points):
         return min(self.starting_health, self.health + healing_points)
@@ -39,3 +30,16 @@ class Hero:
         else:
             mana_value = self.mana + args[0]
             return min(self.starting_mana, mana_value)
+
+    def attack(self, **kwargs):
+        if kwargs['by'] == "weapon":
+            if self.current_weapon is None:
+                return 0
+            else:
+                return self.current_weapon.get_damage()
+
+        if kwargs['by'] == "magic":
+            if self.current_spell is None:
+                return 0
+            else:
+                return self.current_spell.get_damage()
